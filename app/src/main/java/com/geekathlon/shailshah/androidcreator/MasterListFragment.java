8c6865d3651322d30e840a3b6148aa5e09d5b1c1
Your Name <you@example.com>
2017-10-23 10:55:59 -0500

    Master Fragment created

--- app/src/main/java/com/geekathlon/shailshah/androidcreator/MasterListFragment.java
+++ app/src/main/java/com/geekathlon/shailshah/androidcreator/MasterListFragment.java
@@ -1,8 +1,60 @@
 package com.geekathlon.shailshah.androidcreator;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
 /**
  * Created by shailshah on 10/23/17.
  */
 
-public class MasterListFragment {
+public class MasterListFragment extends Fragment{
+    OnImageClickListener mCallback;
+
+    public interface OnImageClickListener {
+        void onImageSelected(int position);
+    }
+
+   public MasterListFragment()
+    {
+
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mCallback = (OnImageClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnImageClickListener");
+        }
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.images_grid_view);
+
+
+      //  MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+
+
+        //gridView.setAdapter(mAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                mCallback.onImageSelected(position);
+            }
+        });
+
+        return rootView;
+    }
+
 }
+
